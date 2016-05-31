6053ce90ef0c42e04225c5df6ec8f236c9f84967
SimonSmale <simon@smalemail.net>
2016-05-31 18:23:37 +0100

    Add DB test

---
+++ Test Scripts/DBTest.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# This script checks to see if the server is up or down. 
+# This info will then be written to a mysql db
+
+import MySQLdb
+import requests
+import time
+
+db = MySQLdb.connect(host="localhost",    # your host, usually localhost
+                     user="ping",         # your username
+                     passwd="websiteping",  # your password
+                     db="Ping")        # name of the data base
+
+# you must create a Cursor object. It will let
+#  you execute all the queries you need
+cur = db.cursor()
+
+while True:
+    response = requests.get('http://ssmale.ddns.net', stream=True)
+    ip = response.raw._connection.sock.getpeername()
+    if response.status_code == requests.codes.ok:
+        print('Server Up')
+        cur.execute("INSERT INTO `Ping`.`ping_history` (`ping_id`, `status`) VALUES (ip, 'Live');")
+    else:
+        print('Server Down')
+        cur.execute("INSERT INTO `Ping`.`ping_history` (`ping_id`, `status`) VALUES (ip, 'Down');")
+    time.sleep(10)
