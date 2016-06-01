daddbc623d0d73f09332113600b1b1e29868f46b
SimonSmale <simon@smalemail.net>
2016-06-01 11:58:06 +0100

    added a new test for getting ip from ping

---
+++ Test Scripts/PingTestIP.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# This script checks to see if the server is up or down. 
+
+import requests
+import socket
+import time
+
+while True:
+    
+    host = 'http://ssmale.ddns.net'
+    
+    ip = socket.gethostbyname('php.iamsimonsmale.co.uk')
+    
+    print ip
+        
+    response = requests.get(host)
+    if response.status_code == requests.codes.ok:
+        print('Server Up')
+    else:
+        print('Server Down')
+    time.sleep(10)
